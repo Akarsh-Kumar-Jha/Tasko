311dc4f01c6adfeccec43afcaae1253ee1177dd1
AKARSH KUMAR <160502447+Akarsh-Kumar-Jha@users.noreply.github.com>
2026-06-10 17:57:52 +0530

    Update geminiConnect.js
    Gemini Model Updated

--- server/config/geminiConnect.js
+++ server/config/geminiConnect.js
@@ -5,6 +5,6 @@ require("dotenv").config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 // Return generative model
-const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
 
 module.exports = model;
